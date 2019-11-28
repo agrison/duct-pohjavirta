3c7c4b850646f0d6f1e6f8146dfbef309c32c0a3
Alexandre Grison <a.grison@gmail.com>
2019-11-28 19:18:49 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:scm :git :deploy-via :clojars}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [metosin/pohjavirta "0.0.1-alpha5"]
-                 [duct/core "0.6.2"]
-                 [duct/logger "0.2.1"]]
+                 [duct/core "0.8.0"]  ; 0.6.2
+                 [duct/logger "0.3.0"]] ; 0.2.1
   :profiles
   {:dev {:dependencies [[clj-http "2.1.0"]]}})
